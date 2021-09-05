547e4836729f6b443b28ff7ed9d1b4d5c369ef10
Oktavianus Chua <oktavianus@gtriipglobal.com>
2021-09-05 21:06:09 +0700

    settings: to add widget_tweaks

--- crypcentra/settings.py
+++ crypcentra/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'ico.apps.IcoConfig',
+    'widget_tweaks',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
