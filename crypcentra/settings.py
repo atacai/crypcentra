7f224a611c0d03bb75e9766a08fa46cbdded4ddb
Oktavianus Chua <oktavianus@gtriipglobal.com>
2021-09-05 21:04:30 +0700

    initiate app

--- crypcentra/settings.py
+++ crypcentra/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'ico.apps.IcoConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
