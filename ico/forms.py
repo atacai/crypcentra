835b8799d671cb88eac96c7655cce51982577df1
Oktavianus Chua <oktavianus@gtriipglobal.com>
2021-09-05 21:09:05 +0700

    forms: to add bid window for auction

--- ico/forms.py
+++ ico/forms.py
@@ -4,6 +4,17 @@ from django.utils.translation import gettext_lazy as _
 from .models import BidWindow, Bid
 
 
+class BidWindowAuctionForm(forms.Form):
+    bid_window_id = forms.IntegerField()
+
+    def save(self):
+        try:
+            window = BidWindow.objects.get(id=self.cleaned_data.get('bid_window_id'))
+        except:
+            raise forms.ValidationError(_('No Bid Window found'))
+        window.auction()
+
+
 class BidForm(forms.ModelForm):
     class Meta:
         model = Bid
