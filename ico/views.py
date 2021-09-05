bed388dbcd2849ab476096a2f3b9af9285aeb699
Oktavianus Chua <oktavianus@gtriipglobal.com>
2021-09-05 21:10:49 +0700

    views: to add index view and bid window list view

--- ico/views.py
+++ ico/views.py
@@ -1,3 +1,21 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
+from django.views.generic import *
 
-# Create your views here.
+from .forms import BidWindowAuctionForm, BidForm
+from .models import BidWindow, Bid
+
+
+class IndexView(RedirectView):
+    pattern_name = 'ico:bid_windows'
+
+
+class BidWindowsView(FormView, ListView):
+    model = BidWindow
+    form_class = BidWindowAuctionForm
+    success_url = '/bid_windows/'
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
+    
